801bdba7bed917945180fd548c939b331d2d391f
Tyler Tseng <airtseng@gmail.com>
2025-06-16 14:20:54 +0800

    Create training branch for online course examples

--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from openai import OpenAI
+from django.db import connection
 
 def http_test(request) -> 'http response':
     value = request.GET['sga']
@@ -61,3 +62,78 @@ def get_ai_response(ingredients) -> str:
         return response.output_text
 
     return 'demo菜色\ndemo作法'
+
+def user_register(request):
+    return render(request,'register.html')
+
+def user_register_add(request):
+    userid = request.POST['userID']
+    username = request.POST['username']
+    password = request.POST['password']
+    truename = request.POST['truename']
+    gender =  request.POST['sex']
+    age = request.POST['age']
+
+    _SQL = 'insert into user_info (userID, username, password, truename, sex, age) values (%s,%s,%s,%s,%s,%s)'
+
+    with connection.cursor() as my_cursor:
+        my_cursor.execute(_SQL,(userid,username,password,truename,gender,age))
+
+    return HttpResponse("註冊成功")
+
+def get_user_info(request):
+    _SQL = 'select *  from user_info'
+
+    with connection.cursor() as my_cursor:
+        my_cursor.execute(_SQL)
+        users_raw = my_cursor.fetchall()
+
+        # print(my_cursor.description)
+        desc = my_cursor.description
+        heads = [ head[0] for head in desc ]
+        users = list()
+        for user in users_raw:
+            dict_user = dict(zip(heads, user))
+            users.append(dict_user)
+
+        # print(heads)
+        # print(users_raw)
+        # print(users)
+
+    return render(request,'userinfo.html',{'users':users})
+
+def user_manage(request):
+    userid = request.GET['userid']
+
+    _SQL = 'select * from user_info where userID = %s'
+    with connection.cursor() as my_cursor:
+        my_cursor.execute(_SQL,(userid,))
+        res = my_cursor.fetchall()
+        user = res[0]
+        # print(user)
+
+    return render(request,'useredit.html',{'user':user})
+
+def user_update(request):
+    userid = request.POST['userID']
+    username = request.POST['username']
+    password = request.POST['password']
+    truename = request.POST['truename']
+    gender = request.POST['sex']
+    age = request.POST['age']
+
+
+    _SQL = 'update user_info set username=%s, password=%s, truename=%s, sex=%s, age=%s where userID = %s'
+    with connection.cursor() as my_cursor:
+        my_cursor.execute(_SQL,(username, password, truename, gender, age, userid))
+
+    return redirect('/user_info/')
+
+def user_delete(request, userid):
+    print(userid)
+
+    _SQL = 'delete from user_info where userID = %s'
+    with connection.cursor() as my_cursor:
+        my_cursor.execute(_SQL,(userid,))
+
+    return redirect('/user_info/')
